bfa436a466dc0996023cf1a42b12a6cf9222bab8
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-03 22:06:12 -0500

    update entrypoint script to delete server.pid

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,5 +10,9 @@ echo "Environment: $RAILS_ENV"
 # --jobs 20 allows for up to 20 parallel gem downloads, --retry 5 allows up to 5 retries
 bundle check || bundle install --jobs 20 --retry 5
 
+# the server creates the file tmp/pids/server.pid whenever it's started. if it is present the server will not start
+# so every time we start the server, we delete it if its there
+rm -rf ./tmp/pids/*
+
 # run whatever commands are passed in the bundle context
 bundle exec ${@}
