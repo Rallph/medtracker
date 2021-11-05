3c16cc97b302e89e34cc43c7c8692eaad6153a33
Cody Anderson <crandersn@protonmail.com>
2021-11-05 15:41:02 -0500

    set up front end skeleton for initial login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root 'session#welcome'
+  post 'session/login'
+  post 'session/logout'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
