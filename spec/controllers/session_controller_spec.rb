37083c2f3607a9f6f7c4be45b2bb9018fb73e1fb
Cody Anderson <crandersn@protonmail.com>
2021-11-05 21:31:06 -0500

    created dummy rspec test to verify functionality

---
+++ spec/controllers/session_controller_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'rails_helper'
+
+
+describe SessionController do
+
+    describe "login" do
+        
+          it "should not fail when user queries logn method" do
+
+            expect(1).to eq(1)
+
+          end
+
+
+    end
+
+end
