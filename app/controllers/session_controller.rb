9133df8feb2589165e0d6fd835fd806687930497
Cody Anderson <crandersn@protonmail.com>
2021-11-07 08:15:39 -0600

    added unit test stubs that define the required functionality for the login story

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,6 +4,15 @@ class SessionController < ApplicationController
   end
 
   def login
+
+    #insert try catch block here
+    username = params.require(:username_field)
+    password = params.require(:password_field)
+
+    # set session to indicate user is logged in
+
+
+
   end
   
   def logout
--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -4,15 +4,36 @@ require 'rails_helper'
 
 describe SessionController do
 
-    describe "login" do
-        
-          it "should not fail when user queries logn method" do
+  describe "login" do
 
-            expect(1).to eq(1)
-
-          end
-
-
+    it "should return to the home screen if the username is missing" do
+      expect(1).to be (2)
     end
+
+    it "should return to the home screen if the password is missing" do
+      expect(1).to be (2)
+    end
+
+    it "should correctly set the session variable" do
+      expect(1).to be (2)
+    end
+
+    it "should redirect to home page for nurses when nurse logs in" do
+      expect(1).to be (2)
+    end
+
+    it "should redirect to home page for admins when admin logs in" do
+      expect(1).to be (2)
+    end
+
+  end
+
+  describe "logout" do
+
+    it "should correclty clear user_id in session variable" do
+      expect(1).to be (2)
+    end
+
+  end
 
 end
