0fa473cbe290f6dfe3a6d6bba7bfc7a0b073b861
Cody Anderson <crandersn@protonmail.com>
2021-11-12 10:29:13 -0600

    commited updates to schema file caused by runing most recent migration:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20211111234336) do
+ActiveRecord::Schema.define(version: 20211112162401) do
+
+  create_table "administrators", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "full_name"
+    t.integer  "school_id"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "administrators", ["email"], name: "index_administrators_on_email", unique: true
+  add_index "administrators", ["reset_password_token"], name: "index_administrators_on_reset_password_token", unique: true
 
   create_table "consent_forms", force: :cascade do |t|
     t.string  "date"
