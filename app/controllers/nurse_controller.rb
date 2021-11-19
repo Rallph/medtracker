d64390abccc025a116eaeddf22bf5d5fb0cc24a8
MottChris <chrismott@uiowa.edu>
2021-11-18 23:42:58 -0600

    first commit with baseline functionality. getting up to date to re synch branch with main

--- app/controllers/nurse_controller.rb
+++ app/controllers/nurse_controller.rb
@@ -4,4 +4,8 @@ class NurseController < ApplicationController
 
   def homepage
   end
+
+  def inventory
+    @school_medications = SchoolMedication.where('school_id = :nurse_school',:nurse_school => current_nurse[:school_id])
+  end
 end
