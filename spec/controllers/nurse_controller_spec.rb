dae87d6b9e3c5c6c705d78943c6d2db737452499
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-19 01:46:09 -0600

    add basic tests for nurse controller

--- spec/controllers/nurse_controller_spec.rb
+++ spec/controllers/nurse_controller_spec.rb
@@ -11,4 +11,37 @@ RSpec.describe NurseController, type: :controller do
     end
   end
 
+  describe "Render Administer Medication Page" do
+    it 'should query the student model' do
+      fake_students = [double('student1'), double('student2')]
+      expect(Student).to receive(:where).and_return(fake_students)
+      get :administer
+    end
+
+    it 'should query the school medication model' do
+      fake_medication = [double('med1'), double('med2')]
+      expect(SchoolMedication).to receive(:where).and_return(fake_medication)
+      get :administer
+    end
+
+    it 'should query the student medication model' do
+      fake_medication = [double('med1'), double('med2')]
+      expect(StudentMedication).to receive(:where).and_return(fake_medication)
+      get :administer
+    end
+
+    it 'should render the administer medication template' do
+
+      fake_students = [double('student1'), double('student2')]
+      fake_medication = [double('med1'), double('med2')]
+
+      allow(Student).to receive(:where).and_return(fake_students)
+      allow(SchoolMedication).to receive(:where).and_return(fake_medication)
+      allow(StudentMedication).to receive(:where).and_return(fake_medication)
+      get :administer
+
+      expect(response).to render_template("administer")
+    end
+  end
+
 end
