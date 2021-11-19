f2b2c9bee16daf9f82d9e423e35991bf9efc87da
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-19 03:22:31 -0600

    query students and school medications, format, and pass to administer view

--- app/controllers/nurse_controller.rb
+++ app/controllers/nurse_controller.rb
@@ -6,5 +6,7 @@ class NurseController < ApplicationController
   end
 
   def administer
+    @student_options = Student.where("school_id = :school_id", { school_id: current_nurse.school_id }).map { |student| [student.full_name, student.id] }
+    @school_medication_options = SchoolMedication.where("school_id = :school_id", { school_id: current_nurse.school_id }).map { |school_medication| [school_medication.medication_name, school_medication.id] }
   end
 end
