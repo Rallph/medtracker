51f5df40b75b5bfbcd6cdae4bacc71f5ae72979c
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-19 03:21:54 -0600

    update nurse controller tests to include methods on mock objects

--- spec/controllers/nurse_controller_spec.rb
+++ spec/controllers/nurse_controller_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe NurseController, type: :controller do
 
-  before { login_with(double('nurse'), :nurse) }
+  before {
+    nurse = double('nurse', school_id: 1)
+    login_with(nurse, :nurse)
+  }
 
   describe "GET #homepage" do
     it "returns http success" do
@@ -13,21 +16,21 @@ RSpec.describe NurseController, type: :controller do
 
   describe "Render Administer Medication Page" do
     it 'should query the student model' do
-      fake_students = [double('student1'), double('student2')]
+      fake_students = [double('student1', full_name: 'student1', id: 1), double('student2', full_name: 'student2', id: 2)]
       expect(Student).to receive(:where).and_return(fake_students)
       get :administer
     end
 
     it 'should query the school medication model' do
-      fake_medication = [double('med1'), double('med2')]
+      fake_medication = [double('med1', medication_name: 'med1', id: 1), double('med2', medication_name: 'med2', id: 2)]
       expect(SchoolMedication).to receive(:where).and_return(fake_medication)
       get :administer
     end
 
     it 'should render the administer medication template' do
 
-      fake_students = [double('student1'), double('student2')]
-      fake_medication = [double('med1'), double('med2')]
+      fake_students = [double('student1', full_name: 'student1', id: 1), double('student2', full_name: 'student2', id: 2)]
+      fake_medication = [double('med1', medication_name: 'med1', id: 1), double('med2', medication_name: 'med2', id: 2)]
 
       allow(Student).to receive(:where).and_return(fake_students)
       allow(SchoolMedication).to receive(:where).and_return(fake_medication)
