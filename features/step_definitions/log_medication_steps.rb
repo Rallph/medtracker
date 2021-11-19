4830ddf4c39766b86260d2b2408936f940fa3973
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-18 21:57:32 -0600

    add step for ensuring fields are present

--- features/step_definitions/log_medication_steps.rb
+++ features/step_definitions/log_medication_steps.rb
@@ -8,3 +8,15 @@ end
 Given /^I am on the Administer Medication page$/ do
   visit :administer
 end
+
+Then /^I should see fields for "(.*?)"$/ do |fields|
+
+  # split strings by comma, replace spaces with dashes to be formatted as HTML IDs
+  field_ids = fields.split(',').map { |field_str| field_str.gsub(" ", "-") }
+
+  # check that the page has elements with those IDs
+  field_ids.each do |field_id|
+    page.has_field?(field_id)
+  end
+
+end
