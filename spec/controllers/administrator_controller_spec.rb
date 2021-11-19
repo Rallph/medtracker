e72ddfcbc3bda5225aa2ef3feb2a74fec2fe74ba
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-11-19 01:35:45 -0600

    add before filter for mock authentication in administrator controller

--- spec/controllers/administrator_controller_spec.rb
+++ spec/controllers/administrator_controller_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe AdministratorController, type: :controller do
 
+  before { login_with(double('admin'), :administrator) }
+
   describe "GET #homepage" do
     it "returns http success" do
       get :homepage
