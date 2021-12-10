25152666c71ff87ecd9194cc8d1647b7096eca22
MottChris <chrismott@uiowa.edu>
2021-12-10 16:36:29 -0600

    removed duplicates for account approvals

--- db/migrate/20211209183430_add_account_approved_to_nurse.rb
+++
@@ -1,9 +0,0 @@
-class AddAccountApprovedToNurse < ActiveRecord::Migration
-  def up
-    add_column :nurses, :account_approved, :boolean, default: false
-  end
-
-  def down
-    remove_column :nurses, :account_approved, :boolean
-  end
-end
--- db/migrate/20211209184428_add_account_approved_to_administrator.rb
+++
@@ -1,9 +0,0 @@
-class AddAccountApprovedToAdministrator < ActiveRecord::Migration
-  def up
-    add_column :administrators, :account_approved, :boolean, default: false
-  end
-
-  def down
-    remove_column :administrators, :account_approved, boolean
-  end
-end
