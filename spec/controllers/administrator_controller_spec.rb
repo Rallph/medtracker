cd491d92a2104573c5c71592e3896dfa41144fda
MottChris <chrismott@uiowa.edu>
2021-12-10 16:33:44 -0600

    changes before switching branch for meeting

--- spec/controllers/administrator_controller_spec.rb
+++ spec/controllers/administrator_controller_spec.rb
@@ -2,12 +2,33 @@ require 'rails_helper'
 
 RSpec.describe AdministratorController, type: :controller do
 
-  before { login_with(double('admin'), :administrator) }
+  #before { login_with(double('admin'), :administrator) }
+  before {
+    administrator = double('administrator', school_id: 1, id: 1)
+    login_with(administrator, :administrator)
+  }
 
   describe "GET #homepage" do
     it "returns http success" do
       get :homepage
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "Render Administrator Inventory Page" do
+    #it "returns http success" do
+      #get :inventory
+      #expect(response).to have_http_status(:success)
+      #end
+
+    it "should query the SchoolMedication Model" do
+      fake_school = double('school1', school_id: 1)
+      fake_medication = [double('medicine1'), double('medicine2')]
+      expect(School).to receive(:find).and_return(fake_school)
+      expect(SchoolMedication).to receive(:where).and_return(fake_medication)
+      get :inventory
+
+      #expect(response).to render_template("inventory")
     end
   end
 
