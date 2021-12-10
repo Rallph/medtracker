b075bbdcc14e8b20728498bff524178421a59d40
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-12-10 13:08:31 -0600

    add additional student medications controller tests

--- spec/controllers/student_medications_controller_spec.rb
+++ spec/controllers/student_medications_controller_spec.rb
@@ -16,6 +16,20 @@ RSpec.describe StudentMedicationsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
+    it 'assigns the medications_for_student instance variable' do
+      allow(StudentMedication).to receive(:where).and_return([])
+      expect(assigns(@medications_for_student))
+      get :get_medications_for_student, { :student_id => "1" }
+    end
+
+    it 'returns the specified students medications in json format' do
+      med_results= [{ id: 1, medication_name: "med", quantity: 10, unit: "unit", school_id: 1, student_id: 1 }]
+      allow(StudentMedication).to receive(:where).and_return(med_results)
+
+      get :get_medications_for_student, { :student_id => "1" }
+      expect(JSON.parse(response.body)).to eq(med_results.map(&:stringify_keys))
+    end
+
 
   end
 
