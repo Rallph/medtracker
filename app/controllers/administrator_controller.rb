2e67c25b2822bf53429201d7115c91c35d3b3602
ClaudioMema <klejdi-mema@uiowa.edu>
2021-12-11 15:19:26 -0600

    added basic input validation for adding a student

--- app/controllers/administrator_controller.rb
+++ app/controllers/administrator_controller.rb
@@ -10,8 +10,21 @@ class AdministratorController < ApplicationController
   end
 
   def submit_new_student
-    emerg_contact = params["emerg_contact"]
+    student_name = params[:student_name]
+    school_id = params[:school_id]
+    date_of_birth = params[:date_of_birth]
+    parent_email = params[:parent_email]
 
+    if student_name.eql? ''
+      flash[:info] = "Student not specified."
+    elsif school_id.eql? ''
+      flash[:info] = "School ID was not specified."
+    elsif date_of_birth.eql? ''
+      flash[:info] = "Date of Birth was not specified."
+    elsif parent_email.eql? ''
+      flash[:info] = "Parent email was not specified."
+    else
+    end
     #Student.create!
 
     flash[:info] = "Student info added successfully"
