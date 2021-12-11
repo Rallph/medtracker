44f71e14ab2658a49368d9820e46cb8eb75a7da4
MottChris <chrismott@uiowa.edu>
2021-12-11 14:48:11 -0600

    cucumber steps updated for parent inventory.

--- features/step_definitions/parent_inventory_steps.rb
+++ features/step_definitions/parent_inventory_steps.rb
@@ -12,10 +12,15 @@ Given /the following "(.*?)" have been added to inventory:/ do |medication_type,
   end
 end
 
-And /the following Parent Student relationship exists:/ do |parents_students_table|
+And /the following Parent Student relationships exists:/ do |parents_students_table|
   parents_students_table.hashes.each do |row|
     parent = parent.find(row["parent_id"])
     student = student.find(row["student_id"])
     parent.students << student
   end
+end
+
+When /^I view "(.*?)" medication inventory$/ do |user_type|
+  #pending # Write code here that turns the phrase above into concrete actions
+  visit "/#{user_type}/inventory"
 end
