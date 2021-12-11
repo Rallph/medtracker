fd53fc898f04774c7114c96a11472bc9f6d0acb8
lukehageman <root@WINDOWS-0S5VHOP.localdomain>
2021-12-10 23:49:47 -0600

    added rspec tests, 100 coverage on parent controller

--- spec/controllers/parent_controller_spec.rb
+++ spec/controllers/parent_controller_spec.rb
@@ -34,4 +34,50 @@ RSpec.describe ParentController, type: :controller do
 
   end
 
+  describe "Render Parent Consent Form" do
+    it 'should query the models: Student, SchoolMedication, and StudentMedication' do
+      fake_approval = [double('fa', student_or_school: 'school', school_medication_id: 1), double('fa', student_or_school: 'student', student_medication_id: 1)]
+      fake_students = [double('student1', full_name: 'student1', id: 1, medication_approvals: fake_approval)] #, double('student2', full_name: 'student2', id: 2)]
+      expect(Student).to receive(:where).and_return(fake_students)
+
+      fake_school_meds = [double('school_med', id: 1),double('school_med', id: 2)]
+      expect(SchoolMedication).to receive(:all).and_return(fake_school_meds)
+
+      fake_student_meds = [double('student_med', id: 1), double('student_med', id: 2)]
+      expect(StudentMedication).to receive(:where).and_return(fake_student_meds)
+
+      get :consent_form, {student_id: 1}
+    end
+  end
+
+  describe "Approve Medication" do
+    it 'should create an instance of the MedicationApproval model' do
+      expect(MedicationApproval).to receive(:create!)
+      post :approve_medication, {student_id: 1, med_type: 'school', sc_medication_id: 1}
+    end
+    it 'should create an instance of the MedicationApproval model' do
+      expect(MedicationApproval).to receive(:create!)
+      post :approve_medication, {student_id: 1, med_type: 'student', st_medication_id: 1}
+    end
+  end
+
+  describe "Disapprove Medication" do
+    it 'should query the MedicationApproval model' do
+      fake_approvals = [double('fa', student_id: 1, student_or_school: 'school', school_medication_id: 1)]
+      expect(MedicationApproval).to receive(:where).and_return(fake_approvals)
+      # allow(MedicationApproval).to receive(:destroy).and_return(false)
+      expect(fake_approvals[0]).to receive(:destroy).with(no_args).and_return(nil)
+
+
+      post :disapprove_medication, {student_id: 1, med_type: 'school', medication_id: 1}
+    end
+
+    it 'should query the MedicationApproval model' do
+      fake_approvals = [double('fa2', student_id: 1, student_or_school: 'school', school_medication_id: 1)]
+      expect(MedicationApproval).to receive(:where).and_return(fake_approvals)
+      expect(fake_approvals[0]).to receive(:destroy).with(no_args).and_return(nil)
+      post :disapprove_medication, {student_id: 1, med_type: 'student', medication_id: 1}
+    end
+  end
+
 end
