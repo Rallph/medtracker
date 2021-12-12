4a55f0bd52fab10cc087e17d7600b475cd7616eb
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-12-11 20:47:27 -0600

    configure app to precompile js for administer page

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Selt2021Proj09
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    config.assets.precompile += %w( ApproveAccounts.js )
+    config.assets.precompile += %w( ApproveAccounts.js AdministerGetStudentMedications.js )
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
