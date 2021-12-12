7bc7d1232c6d7b8912afab31195be61f0c4807da
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-12-12 01:16:31 -0600

    update decrease_medication_quantity to not be a callback

--- app/models/school_medication_transaction.rb
+++ app/models/school_medication_transaction.rb
@@ -3,13 +3,10 @@ class SchoolMedicationTransaction < ActiveRecord::Base
   belongs_to :nurse
   belongs_to :school_medication
 
-  after_create :decrease_medication_quantity
-
-  private
-    def decrease_medication_quantity
-      medication = SchoolMedication.find_by(id: self.school_medication_id)
-      medication.quantity -= self.change_in_quantity.to_i
-      medication.save
-    end
+  def decrease_medication_quantity
+    medication = SchoolMedication.find_by(id: self.school_medication_id)
+    medication.quantity -= self.change_in_quantity.to_i
+    medication.save
+  end
 
 end
