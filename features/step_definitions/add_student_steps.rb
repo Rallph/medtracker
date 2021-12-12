cfb9ca47a41b05f4e4591325c88c3e006d83b833
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-12-12 03:56:08 -0600

    update student features

--- features/step_definitions/add_student_steps.rb
+++ features/step_definitions/add_student_steps.rb
@@ -11,7 +11,7 @@ When /^I enter a student as name: "(.*?)", date_of_birth: "(.*?)", parent_email:
 end
 
 
-When /^I enter a student as name: "(.*?)", school_id: "(.*?)", date_of_birth: "(.*?)", parent_email: "(.*?)"$/ do |name, school_id, date_of_birth, parent_email|
+When /^I input a student as name: "(.*?)", school_id: "(.*?)", date_of_birth: "(.*?)", parent_email: "(.*?)"$/ do |name, school_id, date_of_birth, parent_email|
   fill_in 'student_name', with: name
   fill_in 'school_id', with: school_id
   fill_in 'date_of_birth', with: date_of_birth
