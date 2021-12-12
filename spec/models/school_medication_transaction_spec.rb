e7a5a27cd605623b3be7217d942217cc8e758e4c
Ralph Nahra <ralph-nahra@uiowa.edu>
2021-12-12 00:35:41 -0600

    add spec for school medication transaction decrease quantity

--- spec/models/school_medication_transaction_spec.rb
+++ spec/models/school_medication_transaction_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe SchoolMedicationTransaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "decrease medication quantity" do
+
+    it 'should call the method to find the school medication' do
+      fake_medication = double('medication', :quantity => 10, :quantity= => 9, :save => nil)
+      expect(SchoolMedication).to receive(:find_by).and_return(fake_medication)
+      SchoolMedicationTransaction.create(school_medication_id: 1, change_in_quantity: 1)
+    end
+
+  end
 end
